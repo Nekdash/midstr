98610829f3493edfbbff5ba5a40292b41278db1a
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-18 21:59:04 +0300

    Update itc_rmFreeSpace.cpp

--- itc_rmFreeSpace.cpp
+++ itc_rmFreeSpace.cpp
@@ -1,17 +1,17 @@
 #include "middle_str.h"
 
 string itc_rmFreeSpace(string str){
-long long i = 0;
     string res = "";
-    while(str[i] != '\0'){
-        if((str[i] == 32 && str[i - 1] != 32) || str[i] < 32 || str[i] > 32)
-            res += str[i];
-        i++;
+    for ( long long i = 0; str[i] != '\0'; i++) {
+        if((str[i] == ' ' && str[i - 1] != ' ') || str[i] != ' ')
+            res += str[i]; // все нужное - оставляем
+        
     }
-    if (res[0] == 32)
+    if (res[0] == ' ') // first space
     res = itc_slice_str(res, 1 , itc_len(res) - 1);
 
-    if((res[itc_len(res) - 1]) == 32)
+    if((res[itc_len(res) - 1]) == ' ') // last space
+        
         res = itc_slice_str(res, 0 , itc_len(res) - 2);
 
     return res;
