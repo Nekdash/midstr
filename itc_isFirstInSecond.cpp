d01120c4e172f8ff5f9d506cad90a16672e41f06
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-18 22:47:34 +0300

    Update itc_isFirstInSecond.cpp

--- itc_isFirstInSecond.cpp
+++ itc_isFirstInSecond.cpp
@@ -1,18 +1,20 @@
 #include "middle_str.h"
+bool itc_isFirstInSecond(string s2, string s1){
+  
+  int n1 = itc_len(s1);
+  int n2 = itc_len(s2);
 
+  if (n1 < n2)return false;
 
-bool itc_isFirstInSecond(string s1, string s2){ // наоборот аргументы
-    long long c = 0;
-    for ( long long i = 0; i < itc_len(s2); i++){
-        c = 0;
-        if (s2[i] == s1[0]){
-            for ( long long j = 0; j < itc_len(s1); j++){
-                if( s2[i+ j] == s1[j] )
-                    c++;
-            }
-        }   
-    
-    if(itc_len(s1) == c)
-        return true;}
-    return false;
+  for (int i = 0; i < n1-n2+1; i++){
+    bool check = false;
+    if (s1[i] == s2[0]) {
+      check = true;
+      for (int j = 1; j < n2; j++)
+        if (s1[i + j] != s2[j])check = false;
+    }
+
+    if (check)return true;
+  }
+  return false;
 }
