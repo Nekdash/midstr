69fbe1a9f0d4b15b398c88611b48a4f4481b4629
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-18 21:20:58 +0300

    Update itc_isFirstInSecond.cpp

--- itc_isFirstInSecond.cpp
+++ itc_isFirstInSecond.cpp
@@ -4,9 +4,9 @@
 
 bool itc_isFirstInSecond(string s2, string s1){ // наоборот аргументы
     long long res = -1;
-    for ( int i = 0; i < itc_len(str1); i++){
-        if (itc_slice_str(s1, i, i+itc_len(s2)-1) == s2){
-            return true;
+    for ( int i = 0; i < itc_len(s1); i++){
+        if (itc_slice_str(s1, i, i+itc_len(s2)-1) == s2){ // вырезка на длину искомой строки
+            return true; // если тут - вернуть 1
             break;
         }
     }
