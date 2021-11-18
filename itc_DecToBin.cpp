16bcd90fd874f8bf9931f126374f18c0ec266158
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-18 20:49:10 +0300

    Update itc_DecToBin.cpp

--- itc_DecToBin.cpp
+++ itc_DecToBin.cpp
@@ -2,37 +2,35 @@
 
 
 string itc_num_to_str(long long num){
-    char ch = '#';
+    char ch = ' ';
     string str = "", res = "";
     while(num > 0){
         ch = (num % 10) + 48;
         str =  ch + str;
-        num /=10;
+        num /= 10;
     }
 
     return str;
 }
 
 string itc_DecToBin(string str){
-
-    long long i =0, temp_num = 0 ,num = 0;
     string temp = "", temp2 = "";
+    long long temp_num = 0;
 
 
-
-    while (str[i] != '\0'){
+    for( long long i = 0; str[i] != '\0'; i++ ){
         if ( str[i] < '0' || str[i] > '9'){
-            num = str_to_num(temp);
-            temp2 += itc_decToBase(num, 2) + str[i];
+            
+            temp2 += itc_decToBase(str_to_num(temp), 2) + str[i];
             temp = "";
             }
-            if ( itc_isDigit(str[i])){
-                temp += str[i];
-            }
-        i++;
+        if ( itc_isDigit(str[i])){
+            temp += str[i];
+        }
+        
     }
-    num = str_to_num(temp);
-    temp2 += itc_decToBase(num, 2);
+    
+    temp2 += itc_decToBase(str_to_num(temp), 2);
     return temp2;
 }
 
